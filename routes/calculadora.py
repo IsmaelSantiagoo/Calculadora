180537318c777161f8422b3bcef44fdbd960b358
Ismael Santiago <87278894+IsmaelSantiagoo@users.noreply.github.com>
2024-04-10 19:34:00 -0300

    Delete routes directory

--- routes/calculadora.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-bp = Blueprint('calculadora', __name__)
-
-@bp.route('/calculadora')
-def calculadora():
-    return render_template('calculadora.html')
